ce5e7b9d08532f377f0eaa18bd0825f59beaeef1
Lucas Correa <lucas.dcorrea1@gmail.com>
2019-08-26 19:07:20 -0300

    UpDate header cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 const app = express();
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(cors());
 
 require('./app/controllers/index')(app);
 
